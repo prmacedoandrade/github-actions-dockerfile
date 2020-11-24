c94096073ad99d9f7bdce9a6f3db5a04588a82ed
Paulo Renato Macêdo <prmacedoandrade@gmail.com>
2020-11-24 10:12:47 -0300

    Test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', function (req, res) {
-    res.send('Hello!');
+    res.send('Fala galera!');
 });
 app.listen(3000, function () {
     console.log('Servidor na porta 3000!');
