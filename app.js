904e615da0512cc5c18935c75b8e5b6a67fe664f
Paulo Renato Macêdo <prmacedoandrade@gmail.com>
2020-11-23 22:28:48 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', function (req, res) {
-    res.send('Olá mundo!');
+    res.send('Hello!');
 });
 app.listen(3000, function () {
     console.log('Servidor na porta 3000!');
